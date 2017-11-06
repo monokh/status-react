26a5cd7a7a8d20dc821bf1d98df85db82bd4674c
janherich <jan.herich@gmail.com>
2017-11-06 09:44:52 +0100

    WIP

--- src/status_im/commands/events/loading.cljs
+++ src/status_im/commands/events/loading.cljs
@@ -2,6 +2,8 @@
   (:require [re-frame.core :as re-frame]
             [status-im.utils.handlers :as handlers]
             [status-im.utils.js-resources :as js-resources]
+            [status-im.native-module.core :as status]
+            [status-im.data-store.local-storage :as local-storage]
             [taoensso.timbre :as log]))
 
 (def db
@@ -15,8 +17,23 @@
                            #{:global :group-chats} {"transactor-group" {"send" {}}}
                            #{:global} {"browse" {"browse" {}}}}})
 
-(defn parse-commands)
 
+;; COFX
+(re-frame/reg-cofx
+  :get-local-storage-data
+  (fn [cofx]
+    (assoc cofx :get-local-storage-data local-storage/get-data)))
+
+;; FX
+(re-frame/reg-fx
+  ::parse-jail
+  (fn [jail-id jail-resource]
+    (status/parse-jail
+     jail-id jail-resource
+     (fn [jail-response]
+       (re-frame/dispatch [::process-jail (types/json->clj jail-response)])))))
+
+;; Handlers
 (defn load-commands
   "This function contact and returns effects
   for loading all commands/responses/subscriptions.
@@ -24,10 +41,11 @@
   It's currently working only for bots, eq we are not evaluating
   dapp resources in jail at all."
   [{:keys [whisper-identity bot-url] :as contact}]
-  (if-let [commands-resource (js-resources/get-resource bot-url)]))
+  (if-let [commands-resource (js-resources/get-resource bot-url)]
+    ))
 
 (handlers/register-handler-db
-  ::parse-commands
+  ::process-jail
   [re-frame/trim-v]
-  (fn [db [jail-id parsed-commands]]
+  (fn [db [{:keys [error result]}]]
     ))
